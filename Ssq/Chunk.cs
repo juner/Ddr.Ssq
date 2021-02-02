bbf4559cd1bed4518e1c8c1f11577a3fb182de17
juner <juner@juner.net>
2021-02-02 11:35:21 +0900

    add chunk reader.

--- Ssq/Chunk.cs
+++ Ssq/Chunk.cs
@@ -4,7 +4,7 @@ namespace Ssq
 {
     public class Chunk
     {
-        public int Offset { get; set; }
+        public long Offset { get; set; }
         public ChunkHeader Header { get; set; }
         public uint[] TimeOffsets { get; set; } = Array.Empty<uint>();
         public int[] Tempo_TFPS_Config { get; set; } = Array.Empty<int>();
--- Ssq/ChunkReader.cs
+++ Ssq/ChunkReader.cs
@@ -1,16 +1,57 @@
 ﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace Ssq
 {
     public class ChunkReader : IDisposable
     {
         Stream Stream;
-        public ChunkReader(Stream Stream)
+        readonly bool LeaveOpen;
+        public ChunkReader()
+        public ChunkReader(Stream Stream, bool LeaveOpen)
             => this.Stream = Stream;
+        public IEnumerable<Chunk> ReadToEnd()
+        {
+            var Length = Stream.Length;
+            while (Stream.Position < Length)
+            {
+
+            }
+        }
+        public Chunk Read()
+        {
+            var Chunk = new Chunk();
+            ReadHeader(Chunk);
+        }
+        public ChunkHeader ReadHeader()
+        {
+            var headerSize = Marshal.SizeOf<ChunkHeader>();
+            var buffer = ArrayPool<byte>.Shared.Rent(headerSize);
+            var span = buffer.AsSpan();
+            var readed = Stream.Read(span);
+            if (readed != headerSize)
+                throw new InvalidOperationException();
+            return MemoryMarshal.Read<ChunkHeader>(span);
+        }
+        public void ReadHeader(Chunk Chunk)
+        {
+            var Offset = Stream.Position;
+            var Header = ReadHeader();
+            Chunk.Offset = Offset;
+            Chunk.Header = Header;
+        }
+        public void ReadBody(Chunk Chunk)
+        {
+            if (Chunk.Header is { Type: ChunkType.EndOfFile } or { Length: 0 })
+                return;
+        }
         public void Dispose()
         {
-            throw new NotImplementedException();
+            if (!LeaveOpen)
+                Stream.Dispose();
         }
     }
 }
