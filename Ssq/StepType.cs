c8bd757bc85d8a16bab64199d20ea57ee363582f
juner <juner@juner.net>
2021-02-07 12:50:04 +0900

    StepData の整理

--- Ssq/StepType.cs
+++ Ssq/StepType.cs
@@ -3,25 +3,91 @@ using System.Runtime.Serialization;
 
 namespace Ssq
 {
+    /// <summary>
+    /// Normal Step Types 
+    /// </summary>
     [Flags]
     public enum StepType : byte
     {
+        /// <summary>
+        /// Player1 Left Step ←
+        /// </summary>
         [StepType(StepPlayers.Player1, StepArrows.Left)]
         Player1Left = 0b_0000_0001,
+        /// <summary>
+        /// Player1 Down Step ↓
+        /// </summary>
         [StepType(StepPlayers.Player1, StepArrows.Down)]
         Player1Down = 0b_0000_0010,
+        /// <summary>
+        /// Player1 Up Step ↑
+        /// </summary>
         [StepType(StepPlayers.Player1, StepArrows.Up)]
         Player1Up = 0b_0000_0100,
+        /// <summary>
+        /// Player1 Right Step →
+        /// </summary>
         [StepType(StepPlayers.Player1, StepArrows.Right)]
         Player1Right = 0b_0000_1000,
+        /// <summary>
+        /// Player1 Special Step
+        /// </summary>
+        Player1Special = 0b_0000_1111,
+        /// <summary>
+        /// Player2 Left Step ←
+        /// </summary>
         [StepType(StepPlayers.Player2, StepArrows.Left)]
         Player2Left = 0b_0001_0000,
+        /// <summary>
+        /// Player2 Down Step ↓
+        /// </summary>
         [StepType(StepPlayers.Player2, StepArrows.Down)]
         Player2Down = 0b_0010_0000,
+        /// <summary>
+        /// Player2 Up Step ↑
+        /// </summary>
         [StepType(StepPlayers.Player2, StepArrows.Up)]
         Player2Up = 0b_0100_0000,
+        /// <summary>
+        /// Player2 Right Step 
+        /// </summary>
         [StepType(StepPlayers.Player2, StepArrows.Right)]
         Player2Right = 0b_1000_0000,
+        /// <summary>
+        /// Player2 Special Step
+        /// </summary>
+        Player2Special = 0b_1111_0000,
+
+    }
+    /// <summary>
+    /// Solo Player Step Type
+    /// </summary>
+    public enum SoloStepType : byte
+    {
+        /// <summary>
+        /// Solo Player Left Step ←
+        /// </summary>
+        SoloPlayerLeft = StepType.Player1Left,
+        /// <summary>
+        /// Solo Player Down Step ↓
+        /// </summary>
+        SoloPlayerDown = StepType.Player1Down,
+        /// <summary>
+        /// Solo Player Up Step ↑
+        /// </summary>
+        SoloPlayerUp = StepType.Player1Up,
+        /// <summary>
+        /// Solo Player Right Step →
+        /// </summary>
+        SoloPlayerRight = StepType.Player1Right,
+        /// <summary>
+        /// Solo Player North West Step ↖
+        /// </summary>
+        SoloPlayerNorthWest = StepType.Player2Left,
+        /// <summary>
+        /// Solo Player North East Step ↗
+        /// </summary>
+        SoloPlayerNorthEast = StepType.Player2Up,
     }
     public static class StepTypeExtensions
     {
